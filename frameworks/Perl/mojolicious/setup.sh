9eb06faefc1ae648f560227ec98a4821af8ab2b8
Joel Berger <joel.a.berger@gmail.com>
2014-11-22 19:23:55 -0600

    carton handles PATH on its own

--- frameworks/Perl/mojolicious/setup.sh
+++ frameworks/Perl/mojolicious/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-${PERL_HOME}/bin/carton exec ${PERL_HOME}/bin/hypnotoad ${TROOT}/app.pl
+${PERL_HOME}/bin/carton exec hypnotoad ${TROOT}/app.pl
 
 ## stop:
-# ${PERL_HOME}/bin/carton exec ${PERL_HOME}/bin/hypnotoad -s ${TROOT}/app.pl
+# ${PERL_HOME}/bin/carton exec hypnotoad -s ${TROOT}/app.pl
 
--- frameworks/Perl/mojolicious/stop.sh
+++ frameworks/Perl/mojolicious/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-${PERL_HOME}/bin/carton exec ${PERL_HOME}/bin/hypnotoad -s ${TROOT}/app.pl
+${PERL_HOME}/bin/carton exec hypnotoad -s ${TROOT}/app.pl
 
