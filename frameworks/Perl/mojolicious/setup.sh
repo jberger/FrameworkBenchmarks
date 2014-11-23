a3b3671fcf129463d36be662ce762f1702a23be9
Joel Berger <joel.a.berger@gmail.com>
2014-11-23 09:39:24 -0600

    try with just the daemon

--- frameworks/Perl/mojolicious/setup.sh
+++ frameworks/Perl/mojolicious/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-${PERL_HOME}/bin/carton exec hypnotoad ${TROOT}/app.pl
+${PERL_HOME}/bin/carton exec ${TROOT}/app.pl daemon
 
 ## stop:
 # ${PERL_HOME}/bin/carton exec hypnotoad -s ${TROOT}/app.pl
