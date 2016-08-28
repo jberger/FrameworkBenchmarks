dc15a36ad6e9388e9abd9cf9718b3f8b4535c90e
Joel Berger <joel.a.berger@gmail.com>
2016-08-27 19:53:31 -0500

    genericize the installation of perl versions

--- toolset/setup/linux/languages/perl.sh
+++ toolset/setup/linux/languages/perl.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists ${IROOT}/perl.installed)
+PERL_VERSION=${PERL_VERSION:-"5.18.2"};
+PERL=perl-$PERL_VERSION
+PERL_HOME=${IROOT}/${PERL}
+INSTALLED=${IROOT}/${PERL}.installed
+
+RETCODE=$(fw_exists $INSTALLED)
 [ ! "$RETCODE" == 0 ] || { \
-  source $IROOT/perl.installed
+  source $INSTALLED
   return 0; }
 
-VERSION="5.18"
-PERL=$IROOT/perl-$VERSION
 
 fw_get -o perl-build.pl https://raw.github.com/tokuhirom/Perl-Build/master/perl-build
 # compile with optimizations, n.b. this does not turn on debugging
-perl perl-build.pl -DDEBUGGING=-g 5.18.2 perl-$VERSION 2>&1 | tee $IROOT/perl-install.log | awk '{ if (NR%100 == 0) printf "."}'
+perl perl-build.pl -DDEBUGGING=-g $PERL_VERSION $PERL 2>&1 | tee $IROOT/$PERL-install.log | perl -ne 'print "." unless $. % 100'
 
 fw_get -o cpanminus.pl http://cpanmin.us
-perl-$VERSION/bin/perl cpanminus.pl --notest --no-man-page App::cpanminus
+$PERL_HOME/bin/perl cpanminus.pl --notest --no-man-page App::cpanminus
 # Install only a bare-bones of Perl modules
 # Install others in the per-framework install script or cpanfile
-perl-$VERSION/bin/cpanm -f --notest --no-man-page Carton JSON JSON::XS IO::Socket::IP IO::Socket::SSL
+$PERL_HOME/bin/cpanm -f --notest --no-man-page Carton JSON JSON::XS IO::Socket::IP IO::Socket::SSL
 
-echo "export PERL_HOME=${PERL}" > $IROOT/perl.installed
-echo -e "export PERL_CARTON_PATH=\$TROOT/local" >> $IROOT/perl.installed
-echo -e "export PERL5LIB=\$PERL_CARTON_PATH/lib/perl5" >> $IROOT/perl.installed
-echo -e "export PATH=\$PERL_CARTON_PATH/bin:\$PERL_HOME/bin:\$PATH" >> $IROOT/perl.installed
+echo "export PERL_HOME=${PERL_HOME}" > $INSTALLED
+echo -e "export PERL_CARTON_PATH=\$TROOT/local" >> $INSTALLED
+echo -e "export PERL5LIB=\$PERL_CARTON_PATH/lib/perl5" >> $INSTALLED
+echo -e "export PATH=\$PERL_CARTON_PATH/bin:\$PERL_HOME/bin:\$PATH" >> $INSTALLED
 
-source $IROOT/perl.installed
+source $INSTALLED
